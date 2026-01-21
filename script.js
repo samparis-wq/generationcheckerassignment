6f87d2282156f1f16187a8e975fde7f2c9407eac
samparis-wq <samparisndaka@gmail.com>
2026-01-21 19:14:09 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+body{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+section{
+    width: 350px;
+}
+h1{
+    font-size: 2.5rem;
+}
+label{
+    display: block;
+    font-size: 1.2rem;
+}
+input{
+    font-size: 1.0rem;
+}
+button{
+    margin-top: 45px;
+    font-size: 1.2rem;
+    font-weight: bold;
+    background-color: rgba(0, 0, 255, 0.5);
+    border-radius: 10px;
+}
+#results{
+    width: 350px;
+    font-size: 1.2rem;
+    margin-top: 30px;
+}
